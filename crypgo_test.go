119d0098c7d80aa2b7dc33f70bf4b403032fbe3e
Germano Rizzo <me@germanorizzo.it>
2022-01-20 19:04:34 +0000

    Zstd, chacha20poly1305 and scrypt.

---
+++ crypgo_test.go
@@ -0,0 +1,74 @@
+package crypgo
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"testing"
+)
+
+func Test(t *testing.T) {
+	password := "hello"
+	plaintext := "world"
+
+	cyphertext, err := Encode(password, plaintext)
+	if err != nil {
+		t.Errorf("error in encoding: %v", err)
+		return
+	}
+
+	plaintext2, err := Decode(password, cyphertext)
+	if err != nil {
+		t.Errorf("error in decoding: %v", err)
+		return
+	}
+
+	if plaintext != plaintext2 {
+		t.Error("error in comparing results")
+	}
+}
+
+func load(url string) string {
+	request, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		panic(err)
+	}
+	client := &http.Client{}
+	resp, err := client.Do(request)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	ret, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	return string(ret)
+}
+
+func TestLongString(t *testing.T) {
+	prufrock := load("https://www.gutenberg.org/cache/epub/1459/pg1459.txt")
+	password := "1234567890"
+	cyphertext, err := CompressAndEncode(password, prufrock, 19)
+	if err != nil {
+		t.Errorf("error in encoding: %v", err)
+		return
+	}
+
+	fmt.Printf("%d -> %d\n", len(prufrock), len(cyphertext))
+
+	plaintext2, err := Decode(password, cyphertext)
+	if err != nil {
+		t.Errorf("error in decoding: %v", err)
+		return
+	}
+
+	if len(cyphertext) > len(prufrock) {
+		t.Error("length should be decreasing")
+		return
+	}
+
+	if prufrock != plaintext2 {
+		t.Error("error in comparing results")
+	}
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/proofrock/crypgo
 
 go 1.17
 
-require golang.org/x/crypto v0.0.0-20220112180741-5e0467b6c7ce
+require (
+	github.com/DataDog/zstd v1.5.0
+	golang.org/x/crypto v0.0.0-20220112180741-5e0467b6c7ce
+)
+
+require golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
